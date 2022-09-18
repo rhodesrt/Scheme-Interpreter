ae9479f13b37bce2d138703c60a0802e90b7a749
Tristan Rhodes <rhodestrt@gmail.com>
2022-09-18 14:16:17 -0700

    completed 1_11

---
+++ 1_11.scm
@@ -0,0 +1,22 @@
+#lang sicp
+
+; Recursive vs. Iterative implementation of:
+;  f(n) = n if n < 3 && f(n) = f(n - 1) + 2f(n - 2) + 3f(n - 3) if n >= 3
+
+(define (recursiveF n)
+  (cond ((< n 3) n)
+    (else (+
+            (recursiveF (- n 1))
+            (* 2 (recursiveF (- n 2)))
+            (* 3 (recursiveF (- n 3)))))))
+
+(recursiveF 5)
+
+(define (iterativeStep a b c n)
+  (cond ((= n 2) c)
+    (else (iterativeStep b c (+ c (* 2 b) (* 3 a)) (- n 1)))))
+
+(define (iterativeF n)
+  (iterativeStep 0 1 2 n))
+
+(iterativeF 5)

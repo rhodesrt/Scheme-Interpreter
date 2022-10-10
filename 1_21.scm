175952aa849a1a4c3a293cddb523ff1723d648ec
hzfa <tristan.rhodes@nordstrom.com>
2022-10-10 13:17:20 -0700

    Completed 1_21

---
+++ 1_21.scm
@@ -0,0 +1,22 @@
+#lang sicp
+
+; Use the smallestDivisor procedure to find the smallest divisor of each of the following
+; numbers: 199, 1999, 19999
+
+(define (square n)
+  (* n n))
+
+(define (divides? a b)
+  (= (modulo b a) 0))
+
+(define (findDivisor n testDivisor)
+  (cond ((> (square testDivisor) n) n)
+        ((divides? testDivisor n) testDivisor)
+        (else (findDivisor n  (+ testDivisor 1)))))
+
+(define (smallestDivisor n)
+  (findDivisor n 2))
+
+(smallestDivisor 199)
+(smallestDivisor 1999)
+(smallestDivisor 19999)

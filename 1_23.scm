58f87c94371558e2583a7bd33e746b3c20a70b0e
Tristan Rhodes <rhodestrt@gmail.com>
2022-10-16 15:47:41 -0700

    1_23

---
+++ 1_23.scm
@@ -0,0 +1,51 @@
+#lang sicp
+
+(define (square n)
+  (* n n))
+
+(define (next n)
+  (cond ((= n 2) 3)
+        (else (+ n 2))))
+
+(define (divides? a b)
+  (= (modulo b a) 0))
+
+(define (findDivisor n testDivisor)
+  (cond ((> (square testDivisor) n) n)
+        ((divides? testDivisor n) testDivisor)
+        (else (findDivisor n  (next testDivisor)))))
+
+(define (smallestDivisor n)
+  (findDivisor n 2))
+
+(define (prime? n)
+  (= (smallestDivisor n) n))
+
+(define (timedPrimeTest n)
+  (newline)
+  (display n)
+  (startPrimeTest n (runtime)))
+
+(define (startPrimeTest n startTime)
+  (if (prime? n)
+      (reportPrime (- (runtime) startTime))))
+
+(define (reportPrime elapsedTime)
+  (display " *** ")
+  (display elapsedTime)
+  (newline))
+
+(define (printAndIncrement start count)
+  (timedPrimeTest start)
+  (printThreePrimes (+ start 1) (+ count 1)))
+
+(define (printThreePrimes start count)
+  (if (< count 3)
+      (if (prime? start)
+          (printAndIncrement start count)
+          (printThreePrimes (+ start 1) count))))
+
+(printThreePrimes 1000000000 0)
+(printThreePrimes 10000000000 0)
+(printThreePrimes 100000000000 0)
+(printThreePrimes 1000000000000 0)

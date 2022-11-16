ab0f54f10ff58d6ca92085277f36dbc5b84a2358
hzfa <tristan.rhodes@nordstrom.com>
2022-11-15 18:00:21 -0800

    Completed 1.29

--- 1_29.scm
+++ 1_29.scm
@@ -5,8 +5,31 @@
 (define (cube x)
   (* x x x))
 
-(define (simpsons f a b n)
-  (define (h a b n)
-    (/ (- b a) n))
-  (define (next x)
-    (x + 1)))
+(define (inc n)
+  (+ n 1))
+
+(define (helper k n)
+  (cond ((= k 0) 1)
+        ((= k n) 1)
+        ((even? k) 2)
+        (else 4)))
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (integral f a b n)
+  (define h (/ (- b a) n))
+  (define (yk k) (f (+ a (* k h))))
+  (define (simpsonTerm k)
+    (* (cond ((= k 0) 1)
+             ((= k n) 1)
+             ((even? k) 2)
+             (else 4))
+       (yk k)))
+  (* (/ h 3) (sum simpsonTerm 0.0 inc n)))
+
+(integral cube 0 1 100)
+(integral cube 0 1 1000)

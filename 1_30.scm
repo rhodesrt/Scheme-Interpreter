3695c0578fb7e231d23e040012ada939ddc64440
Tristan Rhodes <rhodestrt@gmail.com>
2022-11-16 22:24:15 -0800

    Completed 1_30

---
+++ 1_30.scm
@@ -0,0 +1,10 @@
+#lang sicp
+
+; Make sum procedure iterative
+
+(define (sum term a next b)
+  (define (iter a result)
+    (if (> a b)
+        result
+        (iter (next a) (+ result (term a)))))
+  (iter a 0))

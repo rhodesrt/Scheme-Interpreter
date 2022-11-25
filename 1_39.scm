af19279b2e72c4e42c447312ad42dcdc2ceeb86d
hzfa <tristan.rhodes@nordstrom.com>
2022-11-25 15:24:30 -0800

    Lambert's procedure can be universally called.

--- 1_39.scm
+++ 1_39.scm
@@ -2,18 +2,19 @@
 
 ; Approximate Lambert's tangent function
 
-(define y 10001)
-
 (define (tangent x k)
-  (if (= k 0)
-      (+ k 1)
-      (- (- y k) (/ (* x x) (tangent x (- k 2))))))
+  (let ((y k))
+    (define (tangentTerm x z)
+      (if (= z 0)
+        (+ z 1)
+        (- (+ 1 (- y z)) (/ (* x x) (tangentTerm x (- z 2))))))
+    (tangentTerm x k)))
 
 (define (lambertTangent x k)
   (/ x (tangent x k)))
 
-(lambertTangent 15.0 10000)
+(lambertTangent 15.0 100000)
 
-(lambertTangent 25.0 10000)
+(lambertTangent 25.0 100000)
 
-(lambertTangent 100.0 10000)
+(lambertTangent 100.0 100000)

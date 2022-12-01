d94b1ba61d68a065f646f580cab271fce57f84a7
Tristan Rhodes <rhodestrt@gmail.com>
2022-11-30 20:11:16 -0800

    Completed 1_44

---
+++ 1_44.scm
@@ -0,0 +1,17 @@
+#lang sicp
+
+(define dx .00001)
+
+(define (smooth f)
+  (lambda (x) (/ (+ (f (+ x dx)) (f x) (f (- x dx))) 3)))
+
+(define (step f n accumulator)
+  (if (= n 0)
+      accumulator
+      (step f (- n 1) (f accumulator))))
+
+(define (repeated f n)
+  (lambda (x) (step f n x)))
+
+(define (nFoldSmooth f n)
+  ((repeated smooth n) f))

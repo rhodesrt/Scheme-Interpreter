dac8b74d8bba6e53d63066af830c29b4713e1d50
Tristan Rhodes <rhodestrt@gmail.com>
2022-11-30 17:51:47 -0800

    Completed 1_43

--- 1_43.scm
+++ 1_43.scm
@@ -2,5 +2,13 @@
 
 ; Write a procedure that takes as inputs a procedure that computes f and a positive integer n and returns the procedure that computes the nth repeated application of f
 
-(define (compose f n x)
-  (lambda (x) (
+(define (step f n accumulator)
+  (if (= n 0)
+      accumulator
+      (step f (- n 1) (f accumulator))))
+
+
+(define (repeated f n)
+  (lambda (x) (step f n x)))
+
+((repeated (lambda (x) (* x x)) 2) 5) ;625
